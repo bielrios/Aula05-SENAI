2c3b06e0abf339d79db3d15b7f7072b0b9819ee2
Gabriel Rios <82127586+bielrios@users.noreply.github.com>
2023-11-10 15:17:44 -0300

    Add files via upload

---
+++ aula05.py
@@ -0,0 +1,26 @@
+# EXERCÍCIO - 1
+
+for seq in range(2, 22, 2):
+  print(seq)
+
+# EXERCÍCIO - 2
+
+for seq in range(5, 55, 5):
+  print(seq)
+print('A soma dessa Range é de:',sum(range(5, 55, 5)))
+
+# EXERCÍCIO - 3
+
+var = 5
+print(type(var))
+
+# EXERCÍCIO - 4
+
+name = input('Digite o seu nome: ')
+print(f'Olá {name}, seja bem vindo(a)!')
+
+# EXERCÍCIO - 5
+
+for numbers in range (1, 11, 2):
+  print(numbers)
+print('A soma dessa Range é de:',sum(range(1,11,2)))
